4371bc39dc8172e686a89b1bd23b1e4564eadbba
Sowmika-Vadivel <sowmika3924@gmail.com>
2024-09-20 11:14:41 +0530

    hii

--- mutuable_immutable.py
+++ mutuable_immutable.py
@@ -2,13 +2,15 @@ items = [(10,20) , 71 , ['a','b'],{1,2,3},'python']
 mutable = []
 immutable = []
 index = 0
-while index < index < len(items):
+while index < len(items):
     item = items[index]
+    index+=1
     if type(item) in (list,set,dict):
         mutable.append(item)
-         print(f"{mutable} is a mutable datatype items present in the string")
     else:
         immutable.append(item)
-        print(f"{immutable} is a immutable datatype items present in the string")
+print(" list is a mutable datatype items present in the string",mutable)
+print(" list is a immutable datatype items present in the string",immutable)
+
 
    
