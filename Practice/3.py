e3472604600baa6fccc5b06ccbef48483e9c9c76
Sowmika-Vadivel <sowmika3924@gmail.com>
2024-09-26 15:49:03 +0530

    q3

---
+++ Practice/3.py
@@ -0,0 +1,5 @@
+# Question - 3
+# Write a program to find the area of a rectangle
+length = int(input("enter length of the rectangle:"))
+breadth= int(input("enter breadth of the rectangle:"))
+print(f"{length*breadth} is the area of the rectangle")
