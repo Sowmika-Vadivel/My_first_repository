01c0987d76d257a429a6e7de0fd42b1a054c1bd1
Sowmika-Vadivel <sowmika3924@gmail.com>
2024-09-26 15:44:19 +0530

    q2

---
+++ Practice/2.py
@@ -0,0 +1,9 @@
+# Question - 2
+# Write a program to find the product of two float numbers
+
+num_1=int(input("enter the number 1 :"))
+num_2=int(input("enter the number 2 :"))
+num_1=float(num_1)
+num_2=float(num_1)
+print(f'{num_1*num_2} is a product of {num_1} & {num_2}')
+
