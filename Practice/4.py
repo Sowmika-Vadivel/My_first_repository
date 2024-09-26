6ae9f12118442df5bdb9f0a23a97b78020b6a2e6
Sowmika-Vadivel <sowmika3924@gmail.com>
2024-09-26 16:00:14 +0530

    q4

---
+++ Practice/4.py
@@ -0,0 +1,8 @@
+# Question - 4
+# Write a program to reverse the given string.
+
+# syntax for Slicing :
+#                       sequence[start:stop:step]
+
+string= input("enter a string :")
+print(f'The reverse of the {string} is {string[::-1]}')
